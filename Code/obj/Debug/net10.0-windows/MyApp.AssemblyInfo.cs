7f8928aab37d59ab0019ebfcc0e63073b712e7b1
Nguyễn Hải Đông <dongnh3452@ut.edu.vn~>
2026-06-14 09:50:30 +0700

    thêm phần giao diện chat và chỉnh sửa docx

--- Code/obj/Debug/net10.0-windows/MyApp.AssemblyInfo.cs
+++ Code/obj/Debug/net10.0-windows/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+743b1e4dfb9deea7d20ba7772dca70c123174bc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ccfaa194a50e3e936658e0e3ea36763cc19562")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
