f339f28273333d7dfd4afecf30b2f436de8ccd1f
Nguyễn Huỳnh Thiên Phước <phuocnht6478@ut.edu.vn>
2026-06-24 17:10:50 +0700

    up code sửa lỗi

--- Code/obj/Debug/net10.0-windows/MyApp.AssemblyInfo.cs
+++ Code/obj/Debug/net10.0-windows/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97ddd58e9df4f8b3f8b9746951564a46f061714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d158c4ba06570f1d550181774f9084154258a759")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
